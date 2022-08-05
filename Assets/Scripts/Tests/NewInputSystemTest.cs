2434f21ea277f2a3884cb6eb17c774135d1dea53
DanP1e <sstelss35@gmail.com>
2022-08-05 16:07:08 +0300

    Passed the control system test for android.

--- Assets/Scripts/Tests/NewInputSystemTest.cs
+++ Assets/Scripts/Tests/NewInputSystemTest.cs
@@ -8,14 +8,23 @@ public class NewInputSystemTest : MonoBehaviour
     [SerializeField] private Text _outputText;
 
     private int _pressesCounter = 0;
+    private int _releasesCounter = 0;
     private TestControls _testControls;
 
     private void Awake()
     {
         _testControls = new TestControls();
         _testControls.AndroidTest.ScreenPress.performed += OnAndroidScreenPressPerformed;
+        _testControls.AndroidTest.ScreenRelease.performed += OnAndroidScreenReleasePerformed;
+
         _testControls.PCTest.ScreenPress.performed += OnPCScreenPressPerformed;
         _testControls.Enable();
+    }
+
+    private void OnAndroidScreenReleasePerformed(InputAction.CallbackContext obj)
+    {
+        _releasesCounter++;
+        _outputText.text = $"Number of releases (Android): {_releasesCounter}";
     }
 
     private void OnPCScreenPressPerformed(InputAction.CallbackContext obj)
